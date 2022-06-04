fe9a45777de74a4127b0542686b7dde8248b1ce1
Alexey Tyurin <6760525@gmail.com>
2022-06-04 16:35:08 -0500

    init commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import './style.css';
+import {sum} from './math';
+
+const div_sum = document.querySelector('.sum');
+div_sum.innerHTML = 'Test Webpack<br>';
+div_sum.innerHTML += 'sum = ' + sum(10, 20);
+
+const btn = document.querySelector('.btn');
+const div_jsonData = document.querySelector('.jsonData');
+
+function getJson() {
+  fetch('http://localhost:3000/contacts/')
+    .then(response => response.json())
+    .then(json => {
+        div_jsonData.innerHTML = '';
+        for (let item of json.values()) {
+            console.log(item);
+            div_jsonData.innerHTML += '<br>';
+            for (let elem in item) {
+                console.log(elem, '=', item[elem]);
+                const newString = `<b>${elem} = ${item[elem]}</b><br>`;
+                div_jsonData.innerHTML += newString;
+            }
+        }
+    })
+    .catch(() => window.alert(`Нет связи с json-server! Запустите сервер командой:\n json-server --watch database.json`));
+}
+btn.addEventListener('click', getJson);
